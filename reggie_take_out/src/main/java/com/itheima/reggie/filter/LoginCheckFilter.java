e4f564d4bee93c109a9dcec4fd8b7b788181486f
GeorgeGao128 <2660674188@qq.com>
2024-07-16 22:34:00 +0800

    Reapply "w"
    This reverts commit 7da97716801dc4339cb09cc007f4e13d2a13a42e.

--- reggie_take_out/src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
+++ reggie_take_out/src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
@@ -1,6 +1,7 @@
 package com.itheima.reggie.filter;
 
 import com.alibaba.fastjson.JSON;
+import com.itheima.reggie.common.BaseContext;
 import com.itheima.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -45,7 +46,9 @@ public class LoginCheckFilter implements Filter {
         }
 
         if(request.getSession().getAttribute("employee") != null){
-            log.info("用户已登录");
+            log.info("用户已登录,用户id为：{}",request.getSession().getAttribute("employee"));
+            Long empId = (Long) request.getSession().getAttribute("employee");
+            BaseContext.setCurrentId(empId);
             filterChain.doFilter(request,response);
             return;
         }
