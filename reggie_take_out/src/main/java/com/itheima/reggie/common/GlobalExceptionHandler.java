7a2667adec72afe65903a285fd93f2f881496818
GeorgeGao128 <2660674188@qq.com>
2024-07-16 22:03:35 +0800

    update

--- reggie_take_out/src/main/java/com/itheima/reggie/common/GlobalExceptionHandler.java
+++ reggie_take_out/src/main/java/com/itheima/reggie/common/GlobalExceptionHandler.java
@@ -28,16 +28,5 @@ public class GlobalExceptionHandler {
         }
         return R.error("未知fail");
     }
-    /**
-     * 异常处理方法
-     * @return
-     */
-    @ExceptionHandler(CustomException.class)
-    public R<String> exceptionHandler(CustomException ex){
-        log.error(ex.getMessage());
-
-        return R.error(ex.getMessage());
-    }
-
 
 }
