3585b8edffe59d0b48cde28fba1dc1bb3d0c4eda
luojiahanfei <147626955+luojiahanfei@users.noreply.github.com>
2024-07-16 22:17:19 +0800

    Revert "Reapply "w""
    This reverts commit 8f93accdf31e671d475a95e7455a09760d9002b8.

--- reggie_take_out/src/main/java/com/itheima/reggie/service/CategroyService.java
+++
@@ -1,8 +0,0 @@
-package com.itheima.reggie.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.itheima.reggie.entity.Category;
-
-public interface CategroyService extends IService<Category>
-{
-}
